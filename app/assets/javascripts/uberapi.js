8ba79710201375334e4e6968edb12a3025da9eda
Alex Pujol <alex.j.pujol@gmail.com>
2016-03-29 15:17:08 -0400

    before making a branch

--- app/assets/javascripts/uberapi.js
+++ app/assets/javascripts/uberapi.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
-    
-    $("form").submit(function(evt) {
-    evt.preventDefault();
 
-    // the AJAX part
-    var uberAPI = "https://api.uber.com/v1/estimates/price";
-    var parameters = {
-        'server_token': '3Y8tpNXfErvmttM5r5xGQmpGh6V-WSoKR-G85JYH',
-        'start_latitude': 40.7218370,
-        'start_longitude': -73.9877070,
-        'end_latitude': 40.7079100,
-        'end_longitude': -74.0064830   
+//    $("form").submit(function(evt) {
+//    evt.preventDefault();
+//    $.ajax({
+//        type: "POST",
+//        url: "https://api.lyft.com/oauth/token",
+//        beforeSend: function(xhr) {
+//            xhr.setRequestHeader("Authorization", "2DR_URI97fjJ:HhDB2zw13ZHO0t_a9pKyanp4Y6z48s2u");   
+//        },
+//        data: {
+//            "grant_type" : "client_credentials",
+//            "scope" : "public"
+//        },
+//        success : function() {console.log(data)},
+//        dataType: "json"
+//    });    
+//    
+//
+//    // the AJAX part
+//    var uberAPI = "https://api.uber.com/v1/estimates/price";
+//    var parameters = {
+//        'server_token': '3Y8tpNXfErvmttM5r5xGQmpGh6V-WSoKR-G85JYH',
+//        'start_latitude': 40.7218370,
+//        'start_longitude': -73.9877070,
+//        'end_latitude': 40.7079100,
+//        'end_longitude': -74.0064830   
     };
   
     function displayData(data) {
