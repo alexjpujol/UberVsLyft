2966127a0a2d1b2b57a22d3485e013d6ac7bdc50
Anton Economos <anton.economos@gmail.com>
2016-03-30 15:24:36 -0400

    Working with Lyft stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
     
+	GEOCODE_ID = "a5785bf3c698677630f375eae20eccb4"
+
 	LYFT_ID = "G_9u7J0dsCUX"
 	LYFT_SECRET = "gSfLysjKqQfJWlBfXvVhj2YXnT6B7wIj"
   # Prevent CSRF attacks by raising an exception.
