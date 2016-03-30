599af8f726a076bbf4621919529e255fdb1aff9e
Shraddha Patel <spatelrb@gmail.com>
2016-03-30 18:00:49 -0400

    pulls lat and long from output

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,31 +1,39 @@
 class HomeController < ApplicationController
     
     def index
+        
+    
+    @uber_request = `curl -H "Authorization: Token "#{UBER_TOKEN}"" \
+    "https://api.uber.com/v1/estimates/price?start_latitude=40.7218370&start_longitude=-73.9877070&end_latitude=40.7079100&end_longitude=-74.0064830"`
+    @uber_output = (JSON.parse(@uber_request))["prices"]
+        
+    @test = @uber_output.is_a? Array
+        
 
-    @obtainToken = `curl -X POST -H "Content-Type: application/json" \
+    @obtainLyftToken = `curl -X POST -H "Content-Type: application/json" \
      --user "#{LYFT_ID}:#{LYFT_SECRET}" \
      -d '{"grant_type": "client_credentials", "scope": "public"}' \
      "https://api.lyft.com/oauth/token"`
 
-	@lyft_response = JSON.parse(@obtainToken)	
-	@lyftToken = @lyft_response["access_token"]
+    @lyft_response = JSON.parse(@obtainLyftToken)   
+    @lyftToken = @lyft_response["access_token"]
 
-	@lyftRequest = `curl --include -X GET -H 'Authorization: Bearer #{@lyftToken}' \
-     "https://api.lyft.com/v1/cost?start_lat=40.7219000&start_lng=-73.9877900&end_lat=40.7079100&end_lng=-74.0064830"`
+    @lyftRequest = `curl --include -X GET -H 'Authorization: Bearer #{@lyftToken}' \
+     "https://api.lyft.com/v1/cost?start_lat=40.707910&start_lng=-74.006483&end_lat=40.721837&end_lng=-73.987707"`
 
-     @splitLyft1 = @lyftRequest.split('version: HTTP/1.1', 2).last
+     @splitLyftRequest = @lyftRequest.split('version: HTTP/1.1', 2)[1]
 
-     @lyftData = JSON.parse(@splitLyft1)
+     @lyftData = JSON.parse(@splitLyftRequest)
      
-	 @lyftLinePriceMax = @lyftData["estimated_cost_cents_max"[3]]
-
-     @testOne = @lyftData.is_a? Object 
-
-    @geocodeurl = open('https://api.opencagedata.com/geocode/v1/json?q=90+John+st,+New+York,+NY,+10038&key=a5785bf3c698677630f375eae20eccb4')
-    
-    @geocoderesponse = JSON.parse(@geocodeurl.read)
-
-    @test = @geocoderesponse.is_a? Object
+     @lyftHash = @lyftData["cost_estimates"]
+     
+     @geocodeurl = open('https://api.opencagedata.com/geocode/v1/json?q=173+ludlow+st,+New+York,+NY,+10002&key=a5785bf3c698677630f375eae20eccb4')
+     
+     @geocoderesponse = JSON.parse(@geocodeurl.read)
+     
+     @lat = @geocoderesponse["results"][2]["geometry"]["lat"]
+     
+     @lng = @geocoderesponse["results"][2]["geometry"]["lng"]
 
     
     end
