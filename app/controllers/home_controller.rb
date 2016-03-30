28f82e46a783ca1e2795df0785a4dcb0c11424fe
Alex Pujol <alex.j.pujol@gmail.com>
2016-03-30 13:24:50 -0400

    added more uber stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,30 @@
 class HomeController < ApplicationController
     
-#    def index
-#        
-#        client = Uber::Client.new do |config|
-#            config.server_token  = "3Y8tpNXfErvmttM5r5xGQmpGh6V-WSoKR-G85JYH"
-#        end
-#        
-#        slat = 40.7218370
-#        slon = -73.9877070
-#        dlat = 40.7079100
-#        dlon = -74.0064830
-#        
-#        puts client.price_estimations(start_latitude: slat, start_longitude: slon, end_latitude: dlat, end_longitude: dlon)
-#        
-#    end
+    def index
+        
+#     @obtainToken = `curl -X POST -H "Content-Type: application/json" \
+#     --user "#{LYFT_ID}:#{LYFT_SECRET}" \
+#     -d '{"grant_type": "client_credentials", "scope": "public"}' \
+#     "https://api.lyft.com/oauth/token"`
+#
+#	@lyft_response = JSON.parse(@obtainToken)	
+#	@lyftToken = @lyft_response["access_token"]
+#
+#	@lyftRequest = `curl --include -X GET -H 'Authorization: Bearer #{@lyftToken}' \
+#     "https://api.lyft.com/v1/cost?start_lat=40.7219000&start_lng=-73.9877900&end_lat=40.7079100&end_lng=-74.0064830"`
+#
+#     @splitLyft1 = @lyftRequest.split('version: HTTP/1.1', 2).last
+#
+#     @lyftData = JSON.parse(@splitLyft1)
+#     
+#	 @lyftLinePriceMax = @lyftData["estimated_cost_cents_max"[3]]
+        
+    
+        @uber_request = `curl -H "Authorization: Token "#{UBER_TOKEN}"" \
+"https://api.uber.com/v1/estimates/price?start_latitude=40.7218370&start_longitude=-73.9877070&end_latitude=40.7079100&end_longitude=-74.0064830"`
+        @uber_response = JSON.parse(@uber_request)
+        @uber_output = @uber_response["prices"][0]
+        
+    end
     
 end
