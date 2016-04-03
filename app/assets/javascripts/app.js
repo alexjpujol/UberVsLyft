80a7ee2c26d5786d53c88bd31ca833581b23363a
Alex Pujol <alex.j.pujol@gmail.com>
2016-04-03 16:23:23 -0400

    added error message for empty search

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,8 @@
-//$(document).ready(function() {
-//    $("form").on("submit", function() {
-//        $("#tables").css("display", "block");
-//    })
-//    
-//})
+$(document).ready(function() {
+    $("form").on("submit", function(event) {
+        if ($("#start").val() === "" || $("#endloc").val() === "") {
+            event.preventDefault();
+            document.getElementById("error").textContent = "Please enter a valid search! You can't have empty values and you can't do mega rides!"
+        };
+    });
+});
