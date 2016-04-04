c91d644c1bbe7811873bc7f3e2bf48fb283a1b4a
Alex Pujol <alex.j.pujol@gmail.com>
2016-04-04 14:58:29 -0400

    before i get master

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-    $("form").on("submit", function(event) {
-        if ($("#start").val() === "" || $("#endloc").val() === "") {
-            event.preventDefault();
-            document.getElementById("error").textContent = "Please enter a valid search! You can't have empty values and you can't do mega rides!"
-        };
-    });
-});
+//$(document).ready(function() {
+//    $("form").on("submit", function(event) {
+//        if ($("#start").val() === "" || $("#endloc").val() === "") {
+//            event.preventDefault();
+//            document.getElementById("error").textContent = "Please enter a valid search! You can't have empty values and you can't do mega rides!"
+//        };
+//    });
+//});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,17 @@ class HomeController < ApplicationController
 
     end
     
-      def show_tables
+    def show_tables
           
-    @start = params[:start]
-    @endloc = params[:endloc]
-    flash[:alert] = "Enter a valid route, dude!"
-    
-        if @start.nil? != true && @endloc.nil? != true     
+        @start = params[:start]
+        @endloc = params[:endloc]
         
+        if @start = "" || @endloc = ""
+
+            flash[:alert] ="Enter a valid route, dude!"
+
+        elsif @start.nil? != true && @endloc.nil? != true     
+
             @geocodeurlstart = open("https://api.opencagedata.com/geocode/v1/json?q=#{@start}&key=#{ENV['GEOCODE_ID']}")
 
             @geocoderesponsestart = JSON.parse(@geocodeurlstart.read)
@@ -30,37 +33,40 @@ class HomeController < ApplicationController
 
 
             @uber_request = `curl -H "Authorization: Token "#{ENV['UBER_TOKEN']}"" \
-        "https://api.uber.com/v1/estimates/price?start_latitude=#{@latstart}&start_longitude=#{@lngstart}&end_latitude=#{@latend}&end_longitude=#{@lngend}"`
+            "https://api.uber.com/v1/estimates/price?start_latitude=#{@latstart}&start_longitude=#{@lngstart}&end_latitude=#{@latend}&end_longitude=#{@lngend}"`
 
             @uber_output = (JSON.parse(@uber_request))["prices"]
 
-            @test = @uber_output.is_a? Array
-
             @obtainLyftToken = `curl -X POST -H "Content-Type: application/json" \
-            --user "#{ENV['LYFT_ID']}:#{ENV['LYFT_SECRET']}" \
-            -d '{"grant_type": "client_credentials", "scope": "public"}' \
-            "https://api.lyft.com/oauth/token"`
+                --user "#{ENV['LYFT_ID']}:#{ENV['LYFT_SECRET']}" \
+                -d '{"grant_type": "client_credentials", "scope": "public"}' \
+                "https://api.lyft.com/oauth/token"`
 
             @lyft_response = JSON.parse(@obtainLyftToken)    
             @lyftToken = @lyft_response["access_token"]
 
 
             @lyftRequest = `curl --include -X GET -H 'Authorization: Bearer #{@lyftToken}' \
-            "https://api.lyft.com/v1/cost?start_lat=#{@latstart}&start_lng=#{@lngstart}&end_lat=#{@latend}&end_lng=#{@lngend}"`
+                "https://api.lyft.com/v1/cost?start_lat=#{@latstart}&start_lng=#{@lngstart}&end_lat=#{@latend}&end_lng=#{@lngend}"`
 
 
             @splitLyftRequest = @lyftRequest.split('version: HTTP/1.1', 2)[1]
 
             @lyftData = JSON.parse(@splitLyftRequest)
 
-             @lyftHash = @lyftData["cost_estimates"]
-        
+            @lyftHash = @lyftData["cost_estimates"]
+
+        else
+
+            flash[:alert] ="Enter a valid route, dude!"
+
         end
-          
-          
+
+
         respond_to do |format|
             format.js
         end  
+
+
     end
-    
 end
